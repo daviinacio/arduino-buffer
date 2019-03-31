82b0fea5240b63c9801dfaca15bd66b6fefd25f2
Davi Inácio <aazz6850@gmail.com>
2019-03-31 20:22:16 -0300

    Buffer implemented

---
+++ Buffer.cpp
@@ -0,0 +1,66 @@
+#include "Buffer.h"
+
+// Constructors
+Buffer::Buffer(int length, int init){
+	this->values = new int[length];
+	this->length = length;
+	this->average = init;
+	this->init = init;
+	this->fill(init);
+}
+
+
+// Getters
+int Buffer::calcAverage(){
+	float sum = 0;
+	
+	for(int i = 0; i < this->length; i++)
+		sum += this->values[i];
+		
+	this->average = sum / this->length;
+		
+	return this->average;
+}
+
+int Buffer::getAverage(){
+	return this->average;
+}
+
+int Buffer::getAt(int position){
+	return this->values[position];
+}
+
+bool Buffer::empty(){
+	return this->average == this->init;	
+}
+
+int Buffer::size(){
+	return this->length;
+}
+
+
+// Setters
+void Buffer::fill(int value){
+	for(int i = 0; i < this->length; i++)
+		this->values[i] = value;
+		
+	this->calcAverage();
+}
+
+void Buffer::insert(int value){
+	if(!this->empty()){
+		// Move array values
+		for(int i = this->length -1; i > 0; i--)
+			this->values[i] = this->values[i - 1];
+		
+		// Insert new value
+		this->values[0] = value;
+		
+		// Update the average variable
+		this->calcAverage();
+	} 
+	// Insert the first value on array
+	else {
+		this->fill(value);
+	}
+}
---
+++ Buffer.h
@@ -0,0 +1,28 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+class Buffer {
+	public:
+		// Constructors
+		Buffer(int, int);
+		
+		// Getters		
+		int calcAverage();
+		int getAverage();
+		int getAt(int);
+		bool empty();
+		int size();
+		
+		// Setters
+		void fill(int);
+		void insert(int);
+		
+	private:
+        int init;
+		int *values;
+		int length;
+		int average;
+};
+
+
+#endif
